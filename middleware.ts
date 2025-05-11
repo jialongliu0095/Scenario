872d0c1522c4b61a436e247377cb0899c203c421
Jialong Liu <jliu0095@uni.sydney.edu.au>
2025-05-11 21:26:23 +1000

    sad1

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,6 @@ export function middleware(request: GeoRequest) {
   const country = request.geo?.country || "US";
   const currentHost = request.headers.get('host');
 
-  // 如果当前请求已经是目标域名，避免再次重定向
   if (country !== "AU" && currentHost !== "scenario-mv2f.vercel.app") {
     return NextResponse.redirect("https://scenario-mv2f.vercel.app");
   }
