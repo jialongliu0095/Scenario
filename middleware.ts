d4e0be084abbbf56621bdd4ecd866c40b6b50755
Jialong Liu <jliu0095@uni.sydney.edu.au>
2025-05-11 21:11:44 +1000

    new

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+interface GeoRequest extends NextRequest {
+  geo?: {
+    country?: string;
+    region?: string;
+    city?: string;
+  };
+}
+
+export function middleware(request: GeoRequest) {
   const country = request.geo?.country || "US";
+
   if (country !== "AU") {
     return NextResponse.redirect("https://global.stratamanagement.com");
   }
+
   return NextResponse.next();
 }
