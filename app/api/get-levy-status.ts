ccea8a88f505cd27158163f52941ad0c32871acf
Jialong Liu <jliu0095@uni.sydney.edu.au>
2025-05-11 21:04:23 +1000

    change

--- app/api/get-levy-status.ts
+++ app/api/get-levy-status.ts
@@ -1,9 +1,14 @@
-export default async function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { ownerId } = req.query;
+  
+
   const levyStatus = {
     ownerId,
     paid: true,
     dueDate: "2025-06-01"
   };
+  
   res.status(200).json(levyStatus);
 }
