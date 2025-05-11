0e4648ae8ae83b5b86e0c9c52ca2366448b15e73
Jialong Liu <jliu0095@uni.sydney.edu.au>
2025-05-11 21:17:52 +1000

    changed

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
+
 interface GeoRequest extends NextRequest {
   geo?: {
     country?: string;
-    region?: string;
-    city?: string;
   };
 }
 
 export function middleware(request: GeoRequest) {
-  const country = request.geo?.country || "US";
-
+  const country = request.geo?.country || "US"; // 
   if (country !== "AU") {
-    return NextResponse.redirect("https://global.stratamanagement.com");
+    return NextResponse.redirect("https://stratamanagement.vercel.app");
   }
 
   return NextResponse.next();
